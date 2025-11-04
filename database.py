557c83d7b867ea0a9d62d961ee5d812b731e9042
HyeongwookKim <herry1021@gmail.com>
2025-11-04 13:50:53 +0900

    FastAPI Init Setting & Test CRUD

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./pybo.db' # 프로젝트 루트 디렉토리에 저장
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    # SQLite는 "파일 기반 데이터베이스"라서, 기본적으로 하나의 스레드에서만 DB에 접근 가능하도록 막아둠
+    # 이러한 제한을 해제해주는 옵션이 ```check_same_thread = False``` -> 즉, 여러 스레드에서 동시에 DB 접근이 가능하도록 설정
+    connect_args = {'check_same_thread': False}
+)
+# autoflush: 쿼리를 실행하기 전에 변경된 내용을 DB 버퍼에 자동 반영할지 여부
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+
+Base = declarative_base() # 데이터베이스 모델 구성 시 사용
---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class Question(Base):
+    __tablename__ = 'question' # 모델에 의해 관리되는 테이블 명
+
+    id = Column(Integer, primary_key = True) # 질문 데이터의 고유 번호 (PK -> 자동 생성됨)
+    subject = Column(String, nullable = False) # 질문 제목
+    content = Column(Text, nullable = False) # 질문 내용
+    create_date = Column(DateTime, nullable = False) # 질문 작성일시
+
+
+class Answer(Base):
+    __tablename__ = 'answer' # 모델에 의해 관리되는 테이블 명
+
+    id = Column(Integer, primary_key = True) # 답변 데이터의 고유 번호 (PK -> 자동 생성됨)
+    content = Column(Text, nullable = False) # 답변 내용
+    create_date = Column(DateTime, nullable = False) # 답변 작성일시
+    question_id = Column(Integer, ForeignKey('question.id', ondelete = 'CASCADE')) # question 테이블의 id 컬럼 참조 (FK) / "CASCADE": 부모가 삭제되면 자식도 같이 삭제됨
+    question = relationship('Question', backref = 'answers') # 답변 모델에서 질문 모델을 참조하기 위함 / backref: 역참조 설정 (즉, 질문에서 답변을 거꾸로 참조)
