f18cbac3a5f5d4e482f79aa4b28d6a5046908838
HyeongwookKim <herry1021@gmail.com>
2025-11-07 10:16:24 +0900

    Add navigation bar and paging function

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -12,15 +12,13 @@ router = APIRouter(
 )
 
 # response_model = list[question_schema.Question] -> question_list 함수의 return 값은 Question 스키마로 구성된 리스트임을 의미
-@router.get('/list', response_model = list[question_schema.Question]) # URL = '/api/question/list'
-def question_list(db: Session = Depends(get_db)):
-    _question_list = question_crud.get_question_list(db)
-
-    ### FastAPI의 Depends를 사용하지 않을 경우, 아래처럼 with 문을 사용하면 됨 ###
-    # with get_db() as db:
-    #     _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
-
-    return _question_list
+@router.get('/list', response_model = question_schema.QuestionList) # URL = '/api/question/list'
+def question_list(db: Session = Depends(get_db), page: int = 0, size: int = 10):
+    total, _question_list = question_crud.get_question_list(db, skip = page * size, limit = size)
+    return {
+        'total': total,
+        'question_list': _question_list
+    }
 
 @router.get('/detail/{question_id}', response_model = question_schema.Question)
 def question_detail(question_id: int, db: Session = Depends(get_db)):
