8b9f497f9ff3eaee12ea68d3f0823b8205d27eef
HyeongwookKim <herry1021@gmail.com>
2025-11-17 09:44:00 +0900

    Frontend build for FastAPI

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+# FileResponse, StaticFiles: 프론트엔드 빌드를 통해 생성한 파일을 FastAPI 서버가 서비스 할 수 있도록 해주기 위함
+from starlette.responses import FileResponse
+from starlette.staticfiles import StaticFiles
 
 from domain.question import question_router
 from domain.answer import answer_router
@@ -24,3 +27,8 @@ app.add_middleware(
 app.include_router(question_router.router)
 app.include_router(answer_router.router)
 app.include_router(user_router.router)
+app.mount('/assets', StaticFiles(directory = 'frontend/dist/assets')) # 경로 매핑
+
+@app.get('/')
+def index(): # '/' 경로로 접속하면, "frontend/dist/index.html" 파일을 읽어서 서비스 할 수 있도록 index 함수 추가
+    return FileResponse('frontend/dist/index.html') # FileResponse: FastAPI가 정적인 파일을 출력할 때 사용
